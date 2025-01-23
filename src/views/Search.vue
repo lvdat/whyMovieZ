d348491ee78fbfcebf6f5635e10257c8f35584f3
lvdat <lvdat13@gmail.com>
2025-01-23 13:31:20 +0700

    fix img

--- src/views/Search.vue
+++ src/views/Search.vue
@@ -1,6 +1,6 @@
 <template>
     <div>
-      <LoadingSpinner :isLoading="isLoading" />
+      <!-- <LoadingSpinner :isLoading="isLoading" /> -->
       <ErrorAlert :errorMessage="errorMessage" />
       <div class="container mx-auto p-4">
         <h1 class="text-2xl font-bold mb-4 dark:text-white">Tìm kiếm phim</h1>
@@ -13,12 +13,14 @@
         />
         <div class="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4 gap-4">
           <div v-for="movie in results" :key="movie._id" class="relative">
-            <img :src="movie.poster_url" :alt="movie.name" class="w-full h-64 object-cover rounded-lg" />
-            <div class="absolute inset-0 bg-black bg-opacity-50 opacity-0 hover:opacity-100 transition-opacity duration-300 rounded-lg p-4">
-              <h2 class="text-white text-lg font-bold">{{ movie.name }}</h2>
-              <p class="text-white text-sm">{{ movie.origin_name }}</p>
-              <p class="text-white text-sm">{{ movie.year }}</p>
-            </div>
+            <router-link :to="'/phim/' + movie.slug">
+                <img :src="'https://phimimg.com/' + movie.poster_url" :alt="movie.name" class="w-full h-64 object-cover rounded-lg" />
+                <div class="absolute inset-0 bg-black bg-opacity-50 opacity-0 hover:opacity-100 transition-opacity duration-300 rounded-lg p-4">
+                <h2 class="text-white text-lg font-bold">{{ movie.name }}</h2>
+                <p class="text-white text-sm">{{ movie.origin_name }}</p>
+                <p class="text-white text-sm">{{ movie.year }}</p>
+                </div>
+            </router-link>
           </div>
         </div>
       </div>
@@ -34,12 +36,12 @@
   
   const keyword = ref('');
   const results = ref([]);
-  const isLoading = ref(false);
+  // const isLoading = ref(false);
   const errorMessage = ref('');
   
   const search = async () => {
     if (keyword.value.length > 2) {
-      isLoading.value = true;
+      //isLoading.value = true;
       errorMessage.value = '';
       try {
         const response = await axios.get(`${API_BASE_URL}/v1/api/tim-kiem?keyword=${keyword.value}&limit=10`);
@@ -48,7 +50,7 @@
         errorMessage.value = DEFAULT_ERROR_MESSAGE;
         console.error('Lỗi khi tải dữ liệu:', error);
       } finally {
-        isLoading.value = false;
+        //isLoading.value = false;
       }
     }
   };
